84a31bb7e5ff708fe25d3207e90865e6aae8c4e6
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-02-26 14:49:18 +0100

    start InvoiceSRV

--- src/main/java/team3/FinalBuildWeek/Invoice/InvoiceSRV.java
+++ src/main/java/team3/FinalBuildWeek/Invoice/InvoiceSRV.java
@@ -3,6 +3,8 @@ package team3.FinalBuildWeek.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class InvoiceSRV {
     @Autowired
@@ -11,5 +13,13 @@ public class InvoiceSRV {
     public Invoice saveInvoice(Invoice newInvoice) {
         invoiceDAO.findById(newInvoice.getId()).ifPresent(invoiceDAO::save);
         return invoiceDAO.save(newInvoice);
+    }
+
+    public Invoice findById(UUID invoiceId) {
+        return invoiceDAO.findById(invoiceId).orElseThrow(() -> new NotFoundException(invoiceId));
+    }
+
+    public Invoice findByIdAndUdate(UUID invoiceId, Invoice modifiedInvoice){
+        Invoice found = this.findById(invoiceId);
     }
 }
