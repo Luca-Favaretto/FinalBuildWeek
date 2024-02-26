536c431b9e68dcd1ffd996dc2cd49ba9a4e01865
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-02-26 15:31:45 +0100

    Start invoice srv

--- src/main/java/team3/FinalBuildWeek/Invoice/InvoiceCTRL.java
+++ src/main/java/team3/FinalBuildWeek/Invoice/InvoiceCTRL.java
@@ -1,4 +1,17 @@
 package team3.FinalBuildWeek.Invoice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invoices")
 public class InvoiceCTRL {
+    @Autowired
+    private InvoiceSRV invoiceSRV;
+
+    @GetMapping
+    public Page<Invoice> getAllInvoice
 }
--- src/main/java/team3/FinalBuildWeek/Invoice/InvoiceDTO.java
+++ src/main/java/team3/FinalBuildWeek/Invoice/InvoiceDTO.java
@@ -1,5 +1,21 @@
 package team3.FinalBuildWeek.Invoice;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
 public record InvoiceDTO() {
-    
+    @NotEmpty(message = "La data della fattura è obbligatoria")
+    @Size(min = 6, max = 8, message = "")
+     private static Date date;
+    @NotEmpty(message = "L'importo della fattura è obbligatorio")
+    @Size(min = )
+    private static double amount;
+    @NotEmpty(message = "Il numero della fattura è obbligatorio")
+    @Size(min = )
+    private static String invoiceNumber;
+    @NotEmpty(message = "Il cliente della fattura è obbligatorio")
+    @Size(min = )
+    private static String customer;
 }
