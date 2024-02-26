e7f8dbd823dffb9f481af8de787e4119cb9af576
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-02-26 11:48:10 +0100

    Initial Commit

---
+++ src/main/java/team3/FinalBuildWeek/FinalBuildWeekApplication.java
@@ -0,0 +1,13 @@
+package team3.FinalBuildWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBuildWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBuildWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team3/FinalBuildWeek/FinalBuildWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.FinalBuildWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBuildWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
