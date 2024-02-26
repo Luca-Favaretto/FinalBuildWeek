802f1b2ed7afe6cbda5ad25be65f1b721ac7b853
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-02-26 15:03:43 +0100

    Service - findByIdAnd Update and Delete

--- src/main/java/team3/FinalBuildWeek/Invoice/InvoiceDTO.java
+++ src/main/java/team3/FinalBuildWeek/Invoice/InvoiceDTO.java
@@ -1,4 +1,5 @@
 package team3.FinalBuildWeek.Invoice;
 
 public record InvoiceDTO() {
+    
 }
--- src/main/java/team3/FinalBuildWeek/Invoice/InvoiceSRV.java
+++ src/main/java/team3/FinalBuildWeek/Invoice/InvoiceSRV.java
@@ -2,6 +2,7 @@ package team3.FinalBuildWeek.Invoice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import team3.FinalBuildWeek.exceptions.NotFoundException;
 
 import java.util.UUID;
 
@@ -19,7 +20,18 @@ public class InvoiceSRV {
         return invoiceDAO.findById(invoiceId).orElseThrow(() -> new NotFoundException(invoiceId));
     }
 
-    public Invoice findByIdAndUdate(UUID invoiceId, Invoice modifiedInvoice){
+    public Invoice findByIdAndUpdate(UUID invoiceId, Invoice modifiedInvoice){
         Invoice found = this.findById(invoiceId);
+        found.setDate(modifiedInvoice.getDate());
+        found.setAmount(modifiedInvoice.getAmount());
+        found.setInvoiceNumber(modifiedInvoice.getInvoiceNumber());
+        found.setInvoiceStatus(modifiedInvoice.getInvoiceStatus());
+        found.setCustomer(modifiedInvoice.getCustomer());
+        return invoiceDAO.save(found);
+    }
+
+    public void findByIdAndDelete(UUID invoiceId){
+        Invoice found = this.findById(invoiceId);
+        invoiceDAO.delete(found);
     }
 }
