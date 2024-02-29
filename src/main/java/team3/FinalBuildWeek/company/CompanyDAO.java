a7e0cdf985ae893f7d0d89e83028e430a523c86d
GiuseppeBarresi98 <145044894+GiuseppeBarresi98@users.noreply.github.com>
2024-02-29 11:17:25 +0100

    .

--- src/main/java/team3/FinalBuildWeek/company/CompanyDAO.java
+++ src/main/java/team3/FinalBuildWeek/company/CompanyDAO.java
@@ -14,6 +14,8 @@ import java.util.UUID;
 public interface CompanyDAO extends JpaRepository<Company, UUID> {
        boolean existsByEmail(String email);
 
+       Company findByBuisnessName(String buisnessName);
+
        @Query("SELECT c.business_name, EXTRACT(YEAR FROM i.date) AS anno, SUM(i.amount) AS fatturatoAnnuo " +
                "FROM Company c " +
                "JOIN Customer cust ON c.customer.id = cust.id " +
@@ -32,6 +34,8 @@ public interface CompanyDAO extends JpaRepository<Company, UUID> {
 
        @Query("SELECT c FROM Company c WHERE LOWER(c.business_name) LIKE %:partialName% ORDER BY c.business_name")
        List<Company> getCompaniesByPartialName(@Param("partialName") String partialName);
+
+
 }
 
 
