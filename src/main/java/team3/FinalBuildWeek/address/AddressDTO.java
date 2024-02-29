3c839d38d773fee870757611d89bac999dbf265e
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-02-29 10:36:48 +0100

    Update AddressDTO.java

--- src/main/java/team3/FinalBuildWeek/address/AddressDTO.java
+++ src/main/java/team3/FinalBuildWeek/address/AddressDTO.java
@@ -2,7 +2,10 @@ package team3.FinalBuildWeek.address;
 
 public record AddressDTO(String address,
                          int number,
-                         String Location,
-                         int PostCode,
-                         String cityHall) {
+
+                         String location,
+                         int postCode,
+                         String municipality
+                         ) {
+
 }
