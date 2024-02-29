ea43dd575e3707e2ef7dd79d99ed892bf2a5156d
GianniCbd <giannicabiddu@gmail.com>
2024-02-29 19:41:59 +0100

    upd loginvalid

--- src/main/java/team3/FinalBuildWeek/auth/authDTO/UserLoginDTO.java
+++ src/main/java/team3/FinalBuildWeek/auth/authDTO/UserLoginDTO.java
@@ -1,11 +1,16 @@
 package team3.FinalBuildWeek.auth.authDTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public record UserLoginDTO(
-//        @NotEmpty(message = "Email is required")
-//        @Email(message = "Email address you provided is not valid.")
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email address you provided is not valid.")
         String email,
 
-//        @NotEmpty(message = "Password is required")
-//        @Size(min = 8, message = "Password must be at least 8 characters long")
+        @NotEmpty(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
         String password) {
 }
